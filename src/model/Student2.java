e5c2a722d2cfbab6b86d2d63f253ac9636280b99
Michael Lee Piotrowski <mlp95747@gmail.com>
2024-07-06 16:54:37 -0700

    small update.

--- src/model/Student2.java
+++ src/model/Student2.java
@@ -4,5 +4,8 @@ public class Student2 extends Student {
 
     public boolean female(){
         return true;
+
+        // To be updated later. (June 6, 2024)
+        // Early version released on GitHub.
     }
 }
